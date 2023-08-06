33232ec099838cde49ea8734f2b8aa50dd948f68
ravin <ravinkarthikacr1998@gmail.com>
2023-08-07 00:07:43 +0530

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent  {
+  data="Happy banking with us"
+  data2="Enter account number"
+  data3="Enter your password"
+
+  
+  login(){
+    alert("Login clicked")
+  }
+
+  acnoChange(event:any){
+    console.log(event.target.value);
+    
+  }
+
+  pswChange(event:any){
+    console.log(event.target.value);
+    
+  }
 
 }
